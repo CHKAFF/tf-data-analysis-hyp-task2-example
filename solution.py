6643bac03522d221203bc52894447fd7a8e5db72
CHKAFF <53139664+CHKAFF@users.noreply.github.com>
2023-04-13 15:56:11 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,10 @@
 import numpy as np
-from scipy.stats import ks_2samp
+from scipy.stats import kruskal, wilcoxon, mannwhitneyu
 
 chat_id = 303247798
 
 def solution(x: np.array, y: np.array) -> bool:
-    stat, p_value = ks_2samp(x, y)
-    return p_value < 0.1
+    _, pval_K = kruskal(x, y)
+    _, pval_W = wilcoxon(x, y)
+    _, pval_M = mannwhitneyu(x, y)
+    return pval_K < 0.1 or pval_W < 0.1 or pval_M < 0.1
