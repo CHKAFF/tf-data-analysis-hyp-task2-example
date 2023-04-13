593610495a0ee12cfeb1c9babafa616a54173cf5
CHKAFF <53139664+CHKAFF@users.noreply.github.com>
2023-04-13 16:47:02 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.stats import kruskal, wilcoxon, mannwhitneyu, ks_2samp
+from scipy.stats import kruskal, wilcoxon, mannwhitneyu, ks_2samp, kendalltau
 
 chat_id = 303247798
 
@@ -8,4 +8,5 @@ def solution(x: np.array, y: np.array) -> bool:
     _, pval_W = wilcoxon(x, y)
     _, pval_M = mannwhitneyu(x, y)
     _, pval_L = ks_2samp(x, y)
-    return pval_K < 0.1 or pval_W < 0.1 or pval_M < 0.1 or pval_L < 0.1
+    _, pval_PP = kendalltau(x, y)
+    return pval_K < 0.1 or pval_W < 0.1 or pval_M < 0.1 or pval_L < 0.1 or pval_PP < 0.1
